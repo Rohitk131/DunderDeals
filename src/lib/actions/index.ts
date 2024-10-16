44a9c33b86b056773e3591422856880e76f6785a
Rohitk131 <rohitkumar13103@gmail.com>
2024-10-16 12:38:50 +0530

    database setup

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,12 +1,45 @@
-"use server"
-import {scrapeAmazonProduct } from '../scraper'; 
-export async function scrapeAndStoreProduct(productUrl: string){
-    if(!productUrl) return; 
+"use server";
 
-    try{
-        const scrapedProduct = await scrapeAmazonProduct(productUrl)
+import { revalidatePath } from "next/cache";
+import { scrapeAmazonProduct } from "../scraper";
+import { connectToDB } from "../mongoose";
+import Product from "@/lib/models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if (!productUrl) return;
+
+  try {
+    connectToDB();
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if (!scrapedProduct) return;
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
     }
-    catch(error: any){
-        throw new Error(`Failed to create/update product: ${error.message}`)
-    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`); 
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`);
+  }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { PriceHistoryItem, Product } from "@/types";
+
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -11,4 +13,65 @@ export function extractPrice(...elements: any){
     if(priceText) return priceText.replace(/\D/g, '');
   }
   return ''; 
+}
+
+
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
+
+export function extractDescription($: any) {
+
+  const selectors = [
+    ".a-unordered-list .a-list-item",
+    ".a-expander-content p",
+    
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  
+  return "";
+}
+
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+
+  return averagePrice;
 }
